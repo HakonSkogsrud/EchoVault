040834d0da0f0b970d498db749be16cc6a2c261b
Håkon Skogsrud <haaksk@gmail.com>
2026-01-06 22:28:54 +0100

    feat: more tools

--- src/mcp_server.py
+++ src/mcp_server.py
@@ -3,23 +3,53 @@ import os
 import duckdb
 from fastmcp import FastMCP
 
-# Create an MCP server named "EchoVault"
 mcp = FastMCP("EchoVault")
 
-# Path to the parquet file
-PARQUET_PATH = os.path.join(os.path.dirname(__file__), "..", "data", "tot.parquet")
+PARQUET_PATH_MUSIC = os.path.join(os.path.dirname(__file__), "..", "data", "music_history.parquet")
+PARQUET_PATH_YOUTUBE = os.path.join(os.path.dirname(__file__), "..", "data", "youtube_history.parquet")
+PARQUET_PATH_SUBSCRIPTIONS = os.path.join(os.path.dirname(__file__), "..", "data", "subscriptions.parquet")
+
 
 
 @mcp.tool()
-def query_data(sql: str) -> str:
+def query_music_history(sql: str) -> str:
     """Execute a SQL query against the database containing music history.
 
     Args:
-        sql: SQL query to execute. Use 'tot' as the table name.
+        sql: SQL query to execute. Use 'music_history' as the table name.
     """
     try:
         conn = duckdb.connect(":memory:")
-        conn.execute(f"CREATE VIEW tot AS SELECT * FROM '{PARQUET_PATH}'")
+        conn.execute(f"CREATE VIEW music_history AS SELECT * FROM '{PARQUET_PATH_MUSIC}'")
+        result = conn.execute(sql).df()
+        return result.to_string()
+    except Exception as e:
+        return f"Error: {str(e)}"
+    
+@mcp.tool()
+def query_youtube_history(sql: str) -> str:
+    """Execute a SQL query against the database containing musiyoutube watch history.
+        Be aware that channel names changes over time, so use channel_id as channel aggregation column. 
+    Args:
+        sql: SQL query to execute. Use 'youtube_history' as the table name.
+    """
+    try:
+        conn = duckdb.connect(":memory:")
+        conn.execute(f"CREATE VIEW youtube_history AS SELECT * FROM '{PARQUET_PATH_YOUTUBE}'")
+        result = conn.execute(sql).df()
+        return result.to_string()
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+@mcp.tool()
+def query_youtube_subscriptions(sql: str) -> str:
+    """Execute a SQL query against the database containing when a channel was subscribed to.
+    Args:
+        sql: SQL query to execute. Use 'youtube_history' as the table name.
+    """
+    try:
+        conn = duckdb.connect(":memory:")
+        conn.execute(f"CREATE VIEW subscriptions AS SELECT * FROM '{PARQUET_PATH_SUBSCRIPTIONS}'")
         result = conn.execute(sql).df()
         return result.to_string()
     except Exception as e:
